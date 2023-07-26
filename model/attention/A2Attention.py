e89894d48eb551ff59e2994c8e771a16c9c2bb35
Huyf9 <98331980+Huyf9@users.noreply.github.com>
2023-07-26 10:02:12 +0800

    update ci_pipeline (#1)
    * Update ci_pipeline.yaml
    
    * update ci_pipeline
    
    * update ci_pipeline
    
    * update pylint
    
    * A2Attention
    
    * ACmixAttention
    
    * CondConv
    
    * DepthwiseSeparableConvolution

--- model/attention/A2Attention.py
+++ model/attention/A2Attention.py
@@ -1,9 +1,16 @@
+"""
+DoubleAttention
+"""
+
 import mindspore as ms
 from mindspore import nn
 from mindspore.common.initializer import initializer, HeNormal, Normal
- 
+
 
 class DoubleAttention(nn.Cell):
+    """
+    Double Attention
+    """
     def __init__(self, in_channels, c_m, c_n, reconstruct=True):
         super().__init__()
         self.in_channels = in_channels
@@ -21,6 +28,7 @@ class DoubleAttention(nn.Cell):
         self.apply(self.init_weights)
 
     def init_weights(self, cell):
+        """ init weight """
         if isinstance(cell, nn.Conv2d):
             cell.weight.set_data(initializer(HeNormal(mode='fan_out'), cell.weight.shape, cell.weight.dtype))
             if cell.bias is not None:
@@ -34,28 +42,27 @@ class DoubleAttention(nn.Cell):
                 cell.bias.set_data(initializer('zeros', cell.bias.shape, cell.bias.dtype))
 
     def construct(self, x):
-        B, C, H, W = x.shape
-        assert C == self.in_channels
+        b, c, h, w = x.shape
+        assert c == self.in_channels
         a = self.convA(x)  # b, c_m, h, w
         b = self.convB(x)  # b, c_n, h, w
         v = self.convV(x)  # b, c_n, h, w
-        tmpA = a.view(B, self.c_m, -1)
-        attention_maps = ms.ops.softmax(b.view(B, self.c_n, -1))
-        attention_vectors = ms.ops.softmax(v.view(B, self.c_n, -1))
+        tmpA = a.view(b, self.c_m, -1)
+        attention_maps = ms.ops.softmax(b.view(b, self.c_n, -1))
+        attention_vectors = ms.ops.softmax(v.view(b, self.c_n, -1))
 
         global_descriptors = ms.ops.bmm(tmpA, attention_maps.permute(0, 2, 1))
 
         tmpZ = ms.ops.matmul(global_descriptors, attention_vectors)
-        tmpZ = tmpZ.view(B, self.c_m, H, W)
+        tmpZ = tmpZ.view(b, self.c_m, h, w)
         if self.reconstruct:
             tmpZ = self.conv_reconstruct(tmpZ)
 
         return tmpZ
-    
+
 
 if __name__ == "__main__":
-    input = ms.ops.randn([12, 512, 7, 7])
+    in_tensor = ms.ops.randn([12, 512, 7, 7])
     a2 = DoubleAttention(512, 128, 128)
-    output = a2(input)
+    output = a2(in_tensor)
     print(output.shape)
-
